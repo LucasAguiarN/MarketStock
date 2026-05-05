fe8f73e6fe3ccf5836f7d8c7a11ff4fd2c405747
LucasAguiarN <lucas_aguiar_nunes@outlook.com>
2026-05-05 19:58:05 -0300

    add metodo to_dict

--- src/Infrastructure/Model/sale.py
+++ src/Infrastructure/Model/sale.py
@@ -21,5 +21,15 @@ class Sale(db.Model):
         self.quantity_sold = quantity_sold
         self.price_at_sale = price_at_sale
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "seller_id": self.seller_id,
+            "quantity_sold": self.quantity_sold,
+            "price_at_sale": self.price_at_sale,
+            "sale_date": self.sale_date.isoformat() if self.sale_date else None
+        }
+
     def __repr__(self):
         return f'<Sale {self.id} - Product: {self.product_id} - Seller: {self.seller_id}>'
